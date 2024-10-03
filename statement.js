6dfbe22343ab519a9003871d68112ac50f843850
Riajul Islam <riyazulislam500@gmail.com>
2024-10-03 20:08:30 +0200

    something changed and added

---
+++ statement.js
@@ -0,0 +1,11 @@
+let x , y, z ;
+x = 5;
+y = 5;
+z = x + y;
+// console.log(z);
+
+let t ,r , a ;
+ t = 'ANDERSON';
+ r = 'ALEX'; 
+ a = t + r ;
+ console.log(a);
---
+++ string.js
@@ -0,0 +1,5 @@
+const fruit = "apple";
+// console.log(fruit);
+
+var fruits ="banana , apple , mango";
+console.log(fruits);
